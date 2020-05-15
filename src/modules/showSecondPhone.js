918fe68cfc1313141aa9191dca81fde8eca60e33
Evgenia Brui <j-lp@mail.ru>
2020-05-15 21:49:18 +0300

    Открытие/закрытие второго телефона

--- src/modules/showSecondPhone.js
+++ src/modules/showSecondPhone.js
@@ -1,12 +1,9 @@
 const showSecondPhone = () => {
     const arrow = document.querySelector('.header-contacts__arrow'),
-        accordPhone = document.querySelector('.header-contacts__phone-number-accord'),
-        secondPhone = document.querySelector('.header-contacts__phone-number-accord a');
+        accordPhone = document.querySelector('.header-contacts__phone-number-accord');
 
     arrow.addEventListener('click', () => {
-        arrow.style.opacity = 0;
-        accordPhone.style.top = '25px';
-        secondPhone.style.opacity = 1;
+        accordPhone.classList.toggle('show-second-phone');
     });
 };
 
