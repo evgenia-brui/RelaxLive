98758f1801c9d769a268884042280d58b4057b0d
Evgenia Brui <j-lp@mail.ru>
2020-05-18 00:35:12 +0300

    Подправила схему работы

--- src/modules/scheme.js
+++ src/modules/scheme.js
@@ -1,6 +1,15 @@
 import tabs from './tabs';
 
 const scheme = () => {
+    const schemeList = document.getElementById("scheme-list"),
+        schemeNav = document.querySelector(".nav"),
+        schemeButtons = schemeList.querySelectorAll("button"),
+        schemeBtnLeft = document.getElementById("nav-arrow-scheme_left"),
+        schemeBtnRight = document.getElementById("nav-arrow-scheme_right");
+
+    let schemeBtnWidth = (schemeList.clientWidth - schemeNav.clientWidth) / schemeButtons.length,
+        schemeStep = 0;
+
     tabs({
         classMainTabs     : '#scheme',
         classButtons      : '#scheme-list',
@@ -9,6 +18,22 @@ const scheme = () => {
         classContents     : '.scheme-slider',
         classContent      : '.scheme-slider__slide',
         classContentActive: 'visible-content-block',
+    });
+
+    schemeBtnLeft.addEventListener("click", (function() {
+        if (schemeStep !== 0) {
+            schemeStep--;
+            schemeList.style.transform = `translateX(-${schemeBtnWidth * schemeStep}px)`;
+        }
+    }));
+    schemeBtnRight.addEventListener("click", (function() {
+        if (schemeStep !== schemeButtons.length) {
+            schemeStep++;
+            schemeList.style.transform = `translateX(-${schemeBtnWidth * schemeStep}px)`;
+        }
+    }));
+    window.addEventListener('resize', () => {
+        schemeBtnWidth = (schemeList.clientWidth - schemeNav.clientWidth) / schemeButtons.length;
     });
 };
 
